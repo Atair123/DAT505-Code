48c86082b290c834333154238751316fa8f09fde
Atair123 <2849279501@qq.com>
2019-03-29 09:58:40 +0800

    hello

--- session5/Work/js/index.js
+++ session5/Work/js/index.js
@@ -1,13 +1,10 @@
 var renderer, scene, camera;
-var cubes = [];
-var randomSpeedX = [];
-var randomSpeedY = [];
 
 function init() {
   scene = new THREE.Scene();
 
   var W = window.innerWidth,
-  H = window.innerHeight;
+      H = window.innerHeight;
 
   camera = new THREE.PerspectiveCamera(45, W / H, .1, 1000);
   camera.position.set(0, 55, 85);
@@ -16,6 +13,13 @@ function init() {
   var spotLight = new THREE.SpotLight(0xFFFFFF);
   spotLight.position.set(0, 1000, 0);
   scene.add(spotLight);
+
+  var ambLight = new THREE.AmbientLight(0xffffff);
+  ambLight.position.set(0, 1000, 0);
+  ambLight.add(spotLight);
+
+  scene.add(ambLight);
+  scene.add(ambLight);
   //spotLight.castShadow = true;
 
   renderer = new THREE.WebGLRenderer({antialias:true});
@@ -23,64 +27,34 @@ function init() {
   renderer.setSize(W, H);
   //renderer.shadowMapEnabled = true;
 
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-
   //Create a two dimensional grid of objects, and position them accordingly
-  for (var x = -10; x < 15; x += 5) { // Start from -35 and sequentially add one every 5 pixels
-    for (var y = -10; y < 15; y += 5) {
-      var boxGeometry = new THREE.BoxGeometry(3, 3, 3);
+  for (var x = -40; x <= 40; x += 5) { // Start from -45 and sequentially add one every 5 pixels
+  for (var y = -40; y <= 40; y += 5) { // Start from -45 and sequentially add one every 5 pixels
+    //for (var y = -30; y <= 30; y += 5) {
+      var boxGeometry = new THREE.BoxGeometry(3, 6, 3);
       //The color of the material is assigned a random color
-      if (x == -5 & y == -5){
-        var boxMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xFFFFFF});
-        var mesh = new THREE.Mesh(boxGeometry, boxMaterial);
-      }
-       else if (x ==5 & y == 5){
-        var boxMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xFFFFFF});
-        var mesh = new THREE.Mesh(boxGeometry, boxMaterial);
-      }
-      else{
-      var boxMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
-      var mesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    }
+      var boxMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xFFFFFF});
+      var mesh1 = new THREE.Mesh(boxGeometry, boxMaterial);
       //mesh.castShadow = true;
 
-      mesh.position.x = x;
-      mesh.position.z = y;
+      mesh1.position.x = x;
+      mesh1.position.y = y;
+      mesh1.position.z = 0;
+      mesh1.scale.y = 0.5;
+      mesh1.rotation.x = 360*Math.random();
+      mesh1.rotation.y = 360*Math.random();
 
-      mesh.scale.y = 0.5;
-
-      mesh.rotation.x = Math.random() *2 *Math.PI;
-      mesh.rotation.y = Math.random() *2 *Math.PI;
-      mesh.rotation.z = Math.random() *2 *Math.PI;
-
-      var radomValueX = (Math.random() * 0.1) - 0.05;
-      //var radomValueY = (Math.random() * 0.1) - 0.05;
-      //var radomValueZ = (Math.random() * 0.1) - 0.05;
-
-      randomSpeedX.push(randomValueX);
-      randomSpeedY.push(randomValueX);
-
-      scene.add(mesh);
-      cubes.push(mesh);
-    }
+      scene.add(mesh1);
   }
-  console.log("Init end");
-  console.log("drawFrame Start ******");
-   document.body.appendChild(renderer.domElement);
+}
+
+  document.body.appendChild(renderer.domElement);
+}
 
 function drawFrame(){
   requestAnimationFrame(drawFrame);
-
-  rot += 0.01;
-
-  //forEach takes all the array entries and passes the c as the object, and i as the index
-  cubes.forEach(function(c, i) {
-  c.rotation.z = rot; //Rotate the object that is referenced in c
-  });
-
   renderer.render(scene, camera);
 }
-
 
 init();
 drawFrame();
